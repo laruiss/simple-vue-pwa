0885a2db30bee59b4c4f85c8b869fc3f9f6eccb6
Stanislas Ormières <laruiss@gmail.com>
2018-04-08 15:22:46 +0200

    Add a simple counter

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <span>Vue.js PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -18,19 +17,10 @@ export default {
 
 <style>
 body {
-  margin: 0;
-}
-
-#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
 }
 
 .main-header {
---
+++ src/components/Counter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button class="button" @click="decrement">-</button>
+    <span class="count">{{counter}}</span>
+    <button class="button" @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialCounter: Number
+  },
+  data () {
+    return {
+      counter: this.initialCounter || 0
+    }
+  },
+  methods: {
+    increment () {
+      this.counter++
+    },
+    decrement () {
+      this.counter--
+    }
+  }
+
+}
+</script>
+
+<style lang="postcss" scoped>
+  .button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-color: var(--primaryColor);
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    width: 2em;
+    height: 2em;
+    margin: 1em;
+  }
+
+  .count {
+    display: inline-block;
+    width: 50px;
+    text-align: center;
+  }
+</style>
